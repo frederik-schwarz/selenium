e537c0b1629999c057702c6378cff845c04ca874
frederik <frederik@jens-schwarz.com>
2023-12-02 16:36:01 +1300

    Add sleep to see open chrome

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+import time
+
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("http://selenium.dev")
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+time.sleep(10)
+
+# driver.implicitly_wait(20)
 
 driver.quit()
