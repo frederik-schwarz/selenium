4a9075460a7f539fa95c6a94b0861c88141dbdfd
frederik <frederik@jens-schwarz.com>
2023-12-02 16:44:13 +1300

    Add fill field and click submit

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-time.sleep(10)
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+text_box.send_keys("this is awsome")
+submit_button.click()
 
 # driver.implicitly_wait(20)
 
